cc51f344b2d43510e0d3bd11bd0740b4465661e7
i4inder <90259823+i4inder@users.noreply.github.com>
2025-03-02 10:14:30 +0530

    Update backend.tf
    Organization name for remote backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
-    # organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>"
+    organization = "i4inder-org"
     workspaces {
       name = "ace-iac-day-two"
     }
